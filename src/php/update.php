dcdfb2a2a84235153ea0647bc2bfc084d1cf5899
skip <skip@192.168.1.11>
2023-05-02 16:09:30 +0700

    perbaiki update.php

--- src/php/update.php
+++ src/php/update.php
@@ -47,31 +47,31 @@ $path = "../../assets/images/jumbotron/" . $jumbotron_image;
 move_uploaded_file($tmp, $path);
 
 // mengubah jumbotron_image
-$query = "UPDATE settings SET value='$jumbotron_image' WHERE name='jumbotron_image'";
-mysqli_query($conn, $query);
+// $query = "UPDATE settings SET value='$jumbotron_image' WHERE name='jumbotron_image'";
+// mysqli_query($conn, $query);
 
 
-// mengambil nama file gambar umkm yang lama
-$query = "SELECT value FROM settings WHERE name='umkm_image'";
-$result = mysqli_query($conn, $query);
-$row = mysqli_fetch_assoc($result);
-$old_umkm_image = $row['value'];
+// // mengambil nama file gambar umkm yang lama
+// $query = "SELECT value FROM settings WHERE name='umkm_image'";
+// $result = mysqli_query($conn, $query);
+// $row = mysqli_fetch_assoc($result);
+// $old_umkm_image = $row['value'];
 
-// menghapus file gambar umkm yang lama
-$path = "../../assets/images/umkm/" . $old_umkm_image;
-if (file_exists($path)) {
-      unlink($path);
-}
+// // menghapus file gambar umkm yang lama
+// $path = "../../assets/images/umkm/" . $old_umkm_image;
+// if (file_exists($path)) {
+//       unlink($path);
+// }
 
-// menyimpan gambar umkm yang baru
-$umkm_image = $_FILES['umkm_image']['name'];
-$tmp = $_FILES['umkm_image']['tmp_name'];
-$path = "../../assets/images/umkm/" . $umkm_image;
-move_uploaded_file($tmp, $path);
+// // menyimpan gambar umkm yang baru
+// $umkm_image = $_FILES['umkm_image']['name'];
+// $tmp = $_FILES['umkm_image']['tmp_name'];
+// $path = "../../assets/images/umkm/" . $umkm_image;
+// move_uploaded_file($tmp, $path);
 
-// mengubah umkm_image
-$query = "UPDATE settings SET value='$umkm_image' WHERE name='umkm_image'";
-mysqli_query($conn, $query);
+// // mengubah umkm_image
+// $query = "UPDATE settings SET value='$umkm_image' WHERE name='umkm_image'";
+// mysqli_query($conn, $query);
 
 
 $result = mysqli_query($conn, "UPDATE `settings` SET 
@@ -81,6 +81,7 @@ $result = mysqli_query($conn, "UPDATE `settings` SET
                                     WHEN 'panel_1'          THEN '$panel_1'
                                     WHEN 'panel_2'          THEN '$panel_2'
                                     WHEN 'panel_3'          THEN '$panel_3'
+                                    WHEN 'jumbotron_image'  THEN '$jumbotron_image'
                                     WHEN 'about_umkm'       THEN '$about_umkm'
                                     WHEN 'instagram'        THEN '$instagram'
                                     WHEN 'whatsapp'         THEN '$whatsapp'
@@ -89,7 +90,7 @@ $result = mysqli_query($conn, "UPDATE `settings` SET
                                     WHEN 'linkedin'         THEN '$linkedin'
                                     WHEN 'twitter'          THEN '$twitter'  
                                     END
-                              WHERE `name` IN ('nama_umkm', 'tagline_umkm', 'panel_1', 'panel_2', 'panel_3', 'about_umkm', 'instagram', 'whatsapp', 'envelope', 'facebook', 'linkedin', 'twitter')
+                              WHERE `name` IN ('nama_umkm', 'tagline_umkm', 'panel_1', 'panel_2', 'panel_3', 'jumbotron_image', 'about_umkm', 'instagram', 'whatsapp', 'envelope', 'facebook', 'linkedin', 'twitter')
                               ");
 
 header('Location: ../../index.php');
